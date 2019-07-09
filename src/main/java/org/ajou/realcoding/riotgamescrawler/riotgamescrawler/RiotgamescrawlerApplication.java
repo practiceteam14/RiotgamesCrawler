70f0391b2437fcd53b1c2d9e9cde1fdb39641064
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-09 17:20:55 +0900

    Add to Project

---
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/RiotgamescrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.riotgamescrawler.riotgamescrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotgamescrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotgamescrawlerApplication.class, args);
+	}
+
+}
