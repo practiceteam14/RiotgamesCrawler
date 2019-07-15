c1b7fb9d870237a44a251367456ec36af0d515eb
Jsnackash <jbjeon0113@naver.com>
2019-07-15 10:39:04 +0900

    Add Basic Project Structure

---
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/api/RiotGamesApiClient.java
@@ -0,0 +1,4 @@
+package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api;
+
+public class RiotGamesApiClient {
+}
---
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
@@ -0,0 +1,4 @@
+package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service;
+
+public class RiotGamesService {
+}
