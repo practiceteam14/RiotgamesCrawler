277bd2ef64b5634dfd198b5a6ce9cc633684a9ba
young <youngjin033@ajou.ac.kr>
2019-07-15 11:02:08 +0900

    Make domain files and add data fields

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/domain/SummonerInfo.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/domain/SummonerInfo.java
@@ -1,4 +1,9 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain;
 
+import lombok.Data;
+
+@Data
 public class SummonerInfo {
+    private String name;
+    private String id;
 }
