f61dc37c3c5de2407025dde198a7cbde70b906ab
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-15 11:18:08 +0900

    Add to getSummonerId method by getMapping in controller

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
@@ -1,4 +1,17 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.controller;
 
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service.RiotGamesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class SummonerInfoController {
+    @Autowired
+    RiotGamesService riotGamesService;
+
+    @GetMapping("/riot-games/summoner-info/{summonerName}")
+    public String getSummonerId(@PathVariable String summonerName) {
+        return riotGamesService.getEncryptedSummonerId(summonerName);
+    }
 }
