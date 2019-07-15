4963b90c8c1ad3f45f8681f6d529bb6a3663899d
young <youngjin033@ajou.ac.kr>
2019-07-15 11:03:48 +0900

    Make request method to get encryptedSummonerId

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/api/RiotGamesApiClient.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/api/RiotGamesApiClient.java
@@ -1,6 +1,10 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api;
 
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RiotGamesApiClient {
@@ -9,6 +13,15 @@ public class RiotGamesApiClient {
     private final String riotGamesSummonerUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={appid}";
     private final String riotGamesLeagueUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={appid}";
 
+    @Autowired
+    RestTemplate restTemplate;
 
-    
+    public String requestSummonerId(String summonerName){
+        return restTemplate.exchange(riotGamesSummonerUrl, HttpMethod.GET, null, SummonerInfo.class, summonerName, appid)
+                .getBody().getId();
+    }
+
+
+
+
 }
