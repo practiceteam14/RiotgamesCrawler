bfb30596853128e14557394032748c589dec09de
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-15 11:31:27 +0900

    Add to getSummonerLeaguePosition method by get in controller

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
@@ -1,6 +1,7 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.controller;
 
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerLeaguePosition;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service.RiotGamesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +13,14 @@ public class SummonerInfoController {
     @Autowired
     RiotGamesService riotGamesService;
 
+    // for checking EncryptedSummonerId
     @GetMapping("/riot-games/summoner-info/{summonerName}")
     public String getSummonerId(@PathVariable String summonerName) {
         return riotGamesService.getEncryptedSummonerId(summonerName);
+    }
+
+    @GetMapping("/riot-games/summoner-league-position/{summonerName}")
+    public SummonerLeaguePosition getSummonerLeaguePosition(@PathVariable String summonerName) {
+        return riotGamesService.getLeaguePosition(summonerName);
     }
 }
