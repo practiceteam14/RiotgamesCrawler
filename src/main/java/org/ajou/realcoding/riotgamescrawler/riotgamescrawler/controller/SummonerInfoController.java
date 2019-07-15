936d16df8a29ca406c87ce03190873a3fc0c4b93
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-15 11:21:39 +0900

    Add to getEncryptedSummonerId method in service

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
@@ -3,9 +3,11 @@ package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.controller;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service.RiotGamesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class SummonerInfoController {
     @Autowired
     RiotGamesService riotGamesService;
--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
@@ -1,4 +1,18 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class RiotGamesService {
+    @Autowired
+    RiotGamesApiClient riotGamesApiClient;
+    public String getEncryptedSummonerId(String summonerName) {
+        String encryptedSummonerId = riotGamesApiClient.requestSummonerId(summonerName);
+        log.info("EncryptedSummonerId : {}", encryptedSummonerId);
+        return encryptedSummonerId;
+    }
 }
