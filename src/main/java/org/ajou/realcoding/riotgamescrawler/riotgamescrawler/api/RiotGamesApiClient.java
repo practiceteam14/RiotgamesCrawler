bce2de17069f83631b6a262d9d40a9a0cd36f782
young <youngjin033@ajou.ac.kr>
2019-07-15 11:05:22 +0900

    Make request method to get summoner's league position

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/api/RiotGamesApiClient.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/api/RiotGamesApiClient.java
@@ -1,10 +1,14 @@
 package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api;
 
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerInfo;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerLeaguePosition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class RiotGamesApiClient {
@@ -19,6 +23,17 @@ public class RiotGamesApiClient {
     public String requestSummonerId(String summonerName){
         return restTemplate.exchange(riotGamesSummonerUrl, HttpMethod.GET, null, SummonerInfo.class, summonerName, appid)
                 .getBody().getId();
+    }
+
+    public SummonerLeaguePosition requestSummonerLeaguePosition(String encryptedSummonerId) {
+        List<SummonerLeaguePosition> summonerLeaguePositions = restTemplate.exchange(riotGamesLeagueUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<SummonerLeaguePosition>>() {}, encryptedSummonerId, appid)
+                .getBody();
+        if(summonerLeaguePositions.size()>0) {
+            return summonerLeaguePositions.get(0);
+        }
+        else {
+            return null;
+        }
     }
 
 
