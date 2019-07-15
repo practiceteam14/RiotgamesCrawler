49b625b515682e5ec4b4da62e3f08d62fd9cd467
Sujin <sujin.k0402@gmail.com>
2019-07-15 12:06:16 +0900

    Changed Annotation

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/controller/SummonerInfoController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SummonerInfoController {
+
     @Autowired
     RiotGamesService riotGamesService;
 
