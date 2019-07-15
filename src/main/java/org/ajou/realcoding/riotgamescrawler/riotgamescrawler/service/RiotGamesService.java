b103687bb6e227113575879e0eda48cff2daacd2
Sujin <sujin.k0402@gmail.com>
2019-07-15 12:09:11 +0900

    Added getCurrentLeaguePositionFromRepository and Edited getLeaguePosition to check if info exist in db

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
@@ -3,6 +3,7 @@ package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service;
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerLeaguePosition;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.repository.SummonerLeaguePositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 public class RiotGamesService {
     @Autowired
     RiotGamesApiClient riotGamesApiClient;
+    @Autowired
+    SummonerLeaguePositionRepository summonerLeaguePositionRepository;
+
     public String getEncryptedSummonerId(String summonerName) {
         String encryptedSummonerId = riotGamesApiClient.requestSummonerId(summonerName);
         log.info("EncryptedSummonerId : {}", encryptedSummonerId);
@@ -19,7 +23,30 @@ public class RiotGamesService {
 
     public SummonerLeaguePosition getLeaguePosition(String summonerName) {
         SummonerLeaguePosition summonerLeaguePosition = riotGamesApiClient.requestSummonerLeaguePosition(getEncryptedSummonerId(summonerName));
-        log.info("SummonerLeaguePosition : {}", summonerLeaguePosition);
+
+        // if league position exist
+        if(summonerLeaguePosition != null){
+            log.info("SummonerLeaguePosition : {}", summonerLeaguePosition);
+
+            if(getCurrentLeaguePositionFromRepository(summonerName) == null){
+                // if not exist in repository, then insert
+                log.info("Does not exist in DB, insert");
+                summonerLeaguePositionRepository.insertSummonerLeaguePosition(summonerLeaguePosition);
+            }
+            else {
+                // if exist in repository, then update
+                log.info("Exist in DB, update");
+                summonerLeaguePositionRepository.updateSummonerLeaguePosition(summonerLeaguePosition);
+            }
+        }
+        else {// if league position does not exist
+            log.info("Summoner League Position info does not exist");
+        }
+
         return summonerLeaguePosition;
+    }
+
+    public SummonerLeaguePosition getCurrentLeaguePositionFromRepository(String summonerName) {
+        return summonerLeaguePositionRepository.currentSummonerLeaguePosition(getEncryptedSummonerId(summonerName));
     }
 }
