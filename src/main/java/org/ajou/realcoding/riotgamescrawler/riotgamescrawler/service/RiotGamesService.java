24911894d2ed37f5b1bac60ee9635b5a755e4edc
JangeSeongHo <qq9725@ajou.ac.kr>
2019-07-15 11:33:41 +0900

    Add to getLeaguePosition method in service

--- src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
+++ src/main/java/org/ajou/realcoding/riotgamescrawler/riotgamescrawler/service/RiotGamesService.java
@@ -2,6 +2,7 @@ package org.ajou.realcoding.riotgamescrawler.riotgamescrawler.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.api.RiotGamesApiClient;
+import org.ajou.realcoding.riotgamescrawler.riotgamescrawler.domain.SummonerLeaguePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,11 @@ public class RiotGamesService {
         String encryptedSummonerId = riotGamesApiClient.requestSummonerId(summonerName);
         log.info("EncryptedSummonerId : {}", encryptedSummonerId);
         return encryptedSummonerId;
+    }
+
+    public SummonerLeaguePosition getLeaguePosition(String summonerName) {
+        SummonerLeaguePosition summonerLeaguePosition = riotGamesApiClient.requestSummonerLeaguePosition(getEncryptedSummonerId(summonerName));
+        log.info("SummonerLeaguePosition : {}", summonerLeaguePosition);
+        return summonerLeaguePosition;
     }
 }
